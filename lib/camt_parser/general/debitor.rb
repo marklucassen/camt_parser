3ca4ca1bbea1deedba9899230def6a65b3118829
Mark Lucassen <mark@marklucassen.nl>
2018-10-08 22:47:06 +0200

    nil checks

--- lib/camt_parser/general/debitor.rb
+++ lib/camt_parser/general/debitor.rb
@@ -5,19 +5,23 @@ module CamtParser
     end
 
     def name
-      @name ||= @xml_data.at_xpath('RltdPties/Dbtr/Nm/text()').text
+		name = @xml_data.at_xpath('RltdPties/Dbtr/Nm/text()')
+		@name ||= name.text unless name.nil?
     end
 
     def iban
-      @iban ||= @xml_data.at_xpath('RltdPties/DbtrAcct/Id/IBAN/text()').text
+    	iban = @xml_data.at_xpath('RltdPties/DbtrAcct/Id/IBAN/text()')
+      @iban ||= iban.text unless iban.nil?
     end
 
     def bic
-      @bic ||= @xml_data.at_xpath('RltdAgts/DbtrAgt/FinInstnId/BIC/text()').text
+    	bic = @xml_data.at_xpath('RltdAgts/DbtrAgt/FinInstnId/BIC/text()')
+		@bic ||= bic.text unless bic.nil?
     end
 
     def bank_name
-      @bank_name ||= @xml_data.at_xpath('RltdAgts/DbtrAgt/FinInstnId/Nm/text()').text
+    	bank_name = @xml_data.at_xpath('RltdAgts/DbtrAgt/FinInstnId/Nm/text()')
+		@bank_name ||= bank_name.text unless bank_name.nil?
     end
   end
 end
