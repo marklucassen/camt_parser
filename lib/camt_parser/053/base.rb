962360f4ade424daeab279dadd188955c720ad12
Mark Lucassen <mark@marklucassen.nl>
2018-10-08 21:29:13 +0200

    fix groupheader?

--- lib/camt_parser/053/base.rb
+++ lib/camt_parser/053/base.rb
@@ -4,8 +4,8 @@ module CamtParser
       attr_reader :group_header, :statements
 
       def initialize(xml_data)
-        # grphdr = xml_data.xpath('BkToCstmrStmt/GrpHdr')
-        # @group_header = GroupHeader.new(grphdr)
+        grphdr = xml_data.at_xpath('BkToCstmrStmt/GrpHdr')
+        @group_header = GroupHeader.new(grphdr)
         # statements = xml_data.xpath('BkToCstmrStmt/Stmt')
         # @statements = statements.map{ |x| Statement.new(x) }
       end
--- lib/camt_parser/version.rb
+++ lib/camt_parser/version.rb
@@ -1,3 +1,3 @@
 module CamtParser
-  VERSION = "4.0.3"
+  VERSION = "4.0.4"
 end
