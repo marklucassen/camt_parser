51e23b63b1bbec8e37417dc354587d7c37229ae9
Mark Lucassen <mark@marklucassen.nl>
2018-10-08 21:18:21 +0200

    comment advanced cide

--- lib/camt_parser/053/base.rb
+++ lib/camt_parser/053/base.rb
@@ -4,10 +4,10 @@ module CamtParser
       attr_reader :group_header, :statements
 
       def initialize(xml_data)
-        grphdr = xml_data.xpath('BkToCstmrStmt/GrpHdr')
-        @group_header = GroupHeader.new(grphdr)
-        statements = xml_data.xpath('BkToCstmrStmt/Stmt')
-        @statements = statements.map{ |x| Statement.new(x) }
+        # grphdr = xml_data.xpath('BkToCstmrStmt/GrpHdr')
+        # @group_header = GroupHeader.new(grphdr)
+        # statements = xml_data.xpath('BkToCstmrStmt/Stmt')
+        # @statements = statements.map{ |x| Statement.new(x) }
       end
     end
   end
